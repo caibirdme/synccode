029cbeeae8242cf7ca262bff0341f5915f9d99f8
caibirdme <englanq@126.com>
2016-07-09 00:26:41 +0800

    extension entry

--- src/extension.js
+++ src/extension.js
@@ -1,9 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import vscode from 'vscode'
+import { commands, window, workspace } from 'vscode'
+import Watcher from './watcher'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+let watcher
+
 export function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -13,11 +17,23 @@ export function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('extension.synccode', function () {
+    const disposable = commands.registerCommand('extension.synccode', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        window.showInformationMessage('Hello World!');
+        let watchOption = {
+            interval: 2,
+            filter: file => {
+                if(file.substring(0,4) === ".git") {
+                    return false
+                }
+                return true
+            },
+            ignoreUnreadableDir: true,
+            ignoreNotPermitted: true,
+        }
+        watcher = new Watcher(workspace.rootPath, watchOption)
     });
 
     context.subscriptions.push(disposable);
@@ -25,4 +41,5 @@ export function activate(context) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+    watcher.Stop()
 }
