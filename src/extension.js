3949708cfbcc4a07b431c77d769e150aef17acf5
caibirmde <englanq@126.com>
2016-07-11 11:23:18 +0800

    change welcome information

--- src/extension.js
+++ src/extension.js
@@ -12,7 +12,7 @@ export function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "synccode" is now active!');
+    //console.log('"synccode" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -21,7 +21,7 @@ export function activate(context) {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        window.showInformationMessage('Hello World!');
+        window.showInformationMessage('"synccode" is now active!');
         let watchOption = {
             interval: 2,
             filter: file => {
